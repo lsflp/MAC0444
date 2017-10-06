6b73a5d0283629d399a91a8c00886a8d4b623816
lsflp <lfdemeloc@gmail.com>
2017-10-06 11:54:50 -0300

    Terminando a lista 2

--- Lista 2/familia.pl
+++ Lista 2/familia.pl
@@ -47,31 +47,47 @@ bisavom(Hom, Pess) :- pai(Hom, X), avom(X, Pess).
 bisavom(Hom, Pess) :- pai(Hom, X), avof(X, Pess).
 
 % Questão d.
-primo_1(P1, P2) :- pai(X, P1), pai(Y, P2), irmaos(X, Y).
-primo_1(P1, P2) :- pai(X, P1), mae(Y, P2), irmaos(X, Y).
-primo_1(P1, P2) :- mae(X, P1), pai(Y, P2), irmaos(X, Y).
-primo_1(P1, P2) :- mae(X, P1), mae(Y, P2), irmaos(X, Y).
+sao_irmaos(X,Y) :- irmaos(X, Y).
+sao_irmaos(X,Y) :- irmaos(Y, X).
 
-primo_1(P1, P2) :- pai(X, P1), pai(Y, P2), irmaos(Y, X).
-primo_1(P1, P2) :- pai(X, P1), mae(Y, P2), irmaos(Y, X).
-primo_1(P1, P2) :- mae(X, P1), pai(Y, P2), irmaos(Y, X).
-primo_1(P1, P2) :- mae(X, P1), mae(Y, P2), irmaos(Y, X).
+primo_1(P1, P2) :- pai(X, P1), pai(Y, P2), sao_irmaos(X, Y).
+primo_1(P1, P2) :- pai(X, P1), mae(Y, P2), sao_irmaos(X, Y).
+primo_1(P1, P2) :- mae(X, P1), pai(Y, P2), sao_irmaos(X, Y).
+primo_1(P1, P2) :- mae(X, P1), mae(Y, P2), sao_irmaos(X, Y).
 
 % Questão e.
 primo(P1, P2) :- primo_1(P1,P2).
 
-primo(P1, P2) :- avom(X, P1), avom(Y, P2), irmaos(X, Y).
-primo(P1, P2) :- avom(X, P1), avof(Y, P2), irmaos(X, Y).
-primo(P1, P2) :- avof(X, P1), avom(Y, P2), irmaos(X, Y).
-primo(P1, P2) :- avof(X, P1), avof(Y, P2), irmaos(X, Y).
-
-primo(P1, P2) :- avom(X, P1), avom(Y, P2), irmaos(Y, X).
-primo(P1, P2) :- avom(X, P1), avof(Y, P2), irmaos(Y, X).
-primo(P1, P2) :- avof(X, P1), avom(Y, P2), irmaos(Y, X).
-primo(P1, P2) :- avof(X, P1), avof(Y, P2), irmaos(Y, X).
+primo(P1, P2) :- pai(X, P1), pai(Y, P2), primo(X, Y).
+primo(P1, P2) :- pai(X, P1), mae(Y, P2), primo(X, Y).
+primo(P1, P2) :- mae(X, P1), pai(Y, P2), primo(X, Y).
+primo(P1, P2) :- mae(X, P1), mae(Y, P2), primo(X, Y).
 
 % Questão f.
 maior_de_idade(Pess) :- idade(Pess, X), X > 17.
 
-% Questão g. 
-pessoas(Lista) :- 
+% Questão g.
+pessoa(X) :- homem(X).
+pessoa(X) :- mulher(X).
+pessoas(Lista) :- findall(X, pessoa(X), Lista).
+
+% Questão h.
+mais_velho(Pess) :- idade(Pess, X), \+ (idade(_, Y), Y > X).
+
+% Questão i.
+seleciona_pessoas(Pess, I, Sexo) :- Sexo = m, homem(Pess), idade(Pess, I).
+seleciona_pessoas(Pess, I, Sexo) :- Sexo = f, mulher(Pess), idade(Pess, I).
+lista_pessoas(Lista, Sexo) :- findall([Pess, I], seleciona_pessoas(Pess, I, Sexo), Lista).
+
+% Questão j.
+parentes(Hom, Mul) :- sao_irmaos(Hom, Mul).
+parentes(Hom, Mul) :- pai(Hom, Mul).
+parentes(Hom, Mul) :- mae(Mul, Hom).
+parentes(Hom, Mul) :- avom(Hom, Mul).
+parentes(Hom, Mul) :- avof(Mul, Hom).
+parentes(Hom, Mul) :- bisavom(Hom, Mul).
+parentes(Hom, Mul) :- primo(Hom, Mul).
+adequados(Hom, Mul) :- homem(Hom), mulher(Mul), idade(Hom, X), idade(Mul, Y), Y < X+3, X < Y+11, \+ (parentes(Hom, Mul)), \+ (casados(Hom, _)), \+ (casados(_, Mul)).
+
+result([_ , E | L], [E | M]) :- result(L , M).
+result(_, []).
